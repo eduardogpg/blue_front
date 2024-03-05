08b90eef351470d55efdc1384b75afa73ca19131
Eduardo <eduardo78d@gmail.com>
2024-03-04 21:31:52 -0600

    Show article detail

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
 import Footer from './Footer';
+import { FaBook, FaEye } from 'react-icons/fa';
+
 
 function Detail() { 
     let { slug } = useParams();
@@ -34,21 +36,23 @@ function Detail() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center min-h-screen">
           <div className="text-center mb-8">
-            <img src="path/to/featured-image.jpg" alt="Featured Image" className="mx-auto w-full mb-8 rounded-lg max-w-xl"/>
-            <h1 className="text-4xl font-bold font-sans text-gray-900 mb-4">{article.title}</h1>
-            <div className="text-gray-600 mb-4">
-              <span>By Author Name</span>
+            <img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1689748656351/fa950764-6dc1-4e30-9d14-051aeb707eb0.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
+              alt="Featured Image" 
+              className="mx-auto w-full mb-8 rounded-lg max-w-xl"
+            />
+            <h1 className="text-5xl font-bold font-sans text-gray-900 mb-4">{article.title}</h1>
+            
+            <div className="flex justify-center items-center space-x-4 mb-4">
+              <span className="font-semibold">By: Eduardo Ismael </span>
+              <span>{new Date(article.created_at).toLocaleDateString()}</span>
+              <div className="flex items-center space-x-2">
+                <span>{article.reading_time} min. read</span>
+                <FaBook className="text-xl" />
+              </div>
             </div>
-            <div className="text-gray-600 mb-4">
-              <span>Level: Medium</span>
-            </div>
-            <div className="flex justify-center space-x-4 mb-4">
-              <a href="#" className="text-gray-600 hover:text-gray-900">Facebook</a>
-              <a href="#" className="text-gray-600 hover:text-gray-900">Twitter</a>
-              <a href="#" className="text-gray-600 hover:text-gray-900">LinkedIn</a>
-            </div>
+
           </div>
-          
+          <hr/>
           <div className="prose lg:prose-xl font-sans bold text-gray-600">
             <p>
               <div 
